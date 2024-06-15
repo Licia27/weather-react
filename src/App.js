6410c77459314a84da4c10205c973bcbed72a129
Licia27 <133221698+Licia27@users.noreply.github.com>
2024-06-15 15:24:56 +0200

    added some css

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ export default function App() {
           </a>{" "}
           and is{" "}
           <a
-            href=""
+            href="https://github.com/Licia27/weather-react"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -27,7 +27,7 @@ export default function App() {
           </a>{" "}
           and{" "}
           <a
-            href=""
+            href="https://vercel.com/new/licia27s-projects/success?developer-id=&external-id=&redirect-url=&branch=master&deploymentUrl=weather-react-hx4b1cz8t-licia27s-projects.vercel.app&projectName=weather-react&s=https%3A%2F%2Fgithub.com%2FLicia27%2Fweather-react&gitOrgLimit=&hasTrialAvailable=1&totalProjects=1&slug=app-future&slug=en-US&slug=new&slug=licia27s-projects&slug=success"
             target="_blank"
             rel="noopener noreferrer"
           >
